0000d80630336e2ea83dbe6755589f1d1f4554a0
unknown <mateichiricea@yahoo.com>
2024-04-23 20:05:20 +0300

    Api

--- api.py
+++ api.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app=FastAPI()
 
+inventory={1:{"Brand":"milbona",
+              "price": 4.50,
+              "taste":"delicious"
+               },
+            2:{"Brand":"milka",
+               "price":3.20,
+               "taste":"mid"}
+          }
 @app.get("/")
 def home():
     return {'data':'test'}
+
+@app.get("/get-item/{item_id}")
+def inv(item_id: int = Path( description="The id of the item you want to preview")):
+    return inventory[item_id]
+
+@app.get("/get-by-brand")
+def inv1(name:str = None):
+    for id_item in inventory:
+        if inventory[id_item]["Brand"] == name:
+            return inventory[id_item]
+    return 'Item not found'
+
+
+
+
 
